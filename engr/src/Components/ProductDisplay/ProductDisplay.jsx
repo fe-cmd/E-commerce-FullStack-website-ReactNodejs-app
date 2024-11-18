0883d4e208885f9cf45f441eb65fec42915e1926
unknown <oluferonmijoshua@gmail.com>
2024-11-18 04:06:11 -0500

    initial commit

--- engr/src/Components/ProductDisplay/ProductDisplay.jsx
+++ engr/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from '../Assets/star_dull_icon.png';
+import { ShopContext } from '../../Context/ShopContext';
 
 const ProductDisplay = (props) => {
     const {product} = props;
+    const {addToCart} = useContext(ShopContext);
   return (
     <div className='productdisplay'>
       <div className='productdisplay-left'>
@@ -15,7 +17,7 @@ const ProductDisplay = (props) => {
           <img src={product.image} alt=''/>
        </div>
        <div className='productdisplay-img'>
-          <img className='productdisplay-main-img' src={product.image}/>
+          <img className='productdisplay-main-img' src={product.image} alt=''/>
        </div>
       </div>
       <div className='productdisplay-right'>
@@ -47,7 +49,7 @@ const ProductDisplay = (props) => {
            <div>XXl</div>
          </div>
         </div>
-        <button>ADD TO CART</button>
+        <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
         <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt, Crop Top</p>
         <p className='productdisplay-right-category'><span>Tags :</span>Modern , Latest</p>
       </div>
